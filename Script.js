db27d7fb4031014e6f6b22880e88dd9f0aef4a9d
Vishwas Chauhan <vchauhan@tavisca.com>
2018-08-22 19:37:32 +0530

    done

--- Script.js
+++ Script.js
@@ -1,107 +1,55 @@
-var count=0;
-function Numberr()
- {
-    var number=document.getElementById("number").value;
-    var re=/^[0-9]{7,11}$/;
-    var node = document.getElementById('number-tooltip')
-    if(!re.test(number))
-    {      
-        node.style.visibility = "visible"
+RegExp.escape = function (s) {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+function suggest() {
+    var arr = ["Pallavi", "Ragul", "Raja", "Anushka", "Bhargavi", "Swinal", "Utkarsh", "Saurabh", "Vishwas", "Paarth", "Gurbaksh", "Ashwarya", "Swapnil", "Ayush", "Shreea", "Smriti", "Shrijeet", "Ankita", "Rajpreet", "Neha", "Nihit", "Mohit"];
+    var count = 0;
+    var input = document.getElementById('value');
+    document.getElementById('list').innerHTML = "";
+    var listItems = document.getElementById('list');
+    var dropdown = document.getElementById('dropdown');
+    dropdown.style.display = 'none';
+    listItems = "";
+    if (input.value) {
+        count = 0;
+        dropdown.style.display = 'block';
+        listItems.size = 3;
+        var showData = input.value;
+        for (var i = 0; i < arr.length; i++) {
+            var testableRegExp = new RegExp(RegExp.escape(showData), "i");
+            if (arr[i].match(testableRegExp)) {
+                addValue(arr[i]);
+            }
+        }
+        if (count == 0) {
+            addValue("No data found");
+        }
+    }
+    function addValue(text) {
         count++;
-    }
-    else
-    {
-        node.style.visibility = "hidden"
-        
-    }
-}
- function FName()
- {
-    var name=document.getElementById("first").value;
-    var re=/^[A-Za-z]+$/;
-    var node = document.getElementById('fname-tooltip') 
-    if(!re.test(name))
-    {
-        node.style.visibility = "visible"
-        count++;     
-    }
-    else
-    {
-        node.style.visibility = "hidden" 
+        var node = document.getElementById('dropdown')
+        if (count > 5) {
+            node.style.overflow = "auto";
+        }
+        listItems = document.getElementById('list');
+        var list = document.createElement("li");
+        list.setAttribute("value", text);
+        list.setAttribute("onclick", "setVal(this.innerHTML)");
+        list.setAttribute("class", "list-items");
+        list.innerHTML = text;
+        listItems.appendChild(list);
     }
 }
-function LName()
- {
-    var name=document.getElementById("last").value;
-    var re=/^[A-Za-z]+$/;
-    var node = document.getElementById('lname-tooltip')
-    if(!re.test(name))
-    {      
-        node.style.visibility ="visible" 
-        count++;
-    }
-    else
-    {
-        node.style.visibility = "hidden" 
-    }
+function setVal(x) {
+    document.getElementById('value').value = x;
+    document.getElementById('dropdown').style.display = 'none';
 }
-function city()
-{
-    var city=document.getElementById("select").value;
-    var node = document.getElementById('select-tooltip')
-    if(city.length==0)
-    {
-        node.style.visibility = "visible"
-        count++;
-        
-    }
-    else
-    {
-        node.style.visibility = "hidden"
-    }
-}
-function Email()
- {
-    var email=document.getElementById("mail").value;
-    var re=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    var node = document.getElementById('mail-tooltip')
-    if(!re.test(email))
-    {      
-        node.style.visibility = "visible"
-        count++;
-    }
-    else
-    {
-        node.style.visibility = "hidden" 
-    }
-}
-function gender()
- {
-    var gender=document.getElementsByName("gender");
-    var node = document.getElementById('gender-tooltip')
-    if(gender[0].checked== false && gender[1].checked == false && gender[2].checked== false)
-    {      
-        node.style.visibility = "visible"
-        count++;
-    }
-    else
-    {
-        node.style.visibility = "hidden" 
-    }
-}
-function CheckAll()
-{
-    count=0;
-    Numberr()
-    FName()
-    LName()
-    Email()
-    city()
-    gender()
-    if(count>0)
-    return false;
-    else
-    return true;
-    
-   
-}
+window.onload = function(){
+    var divToHide = document.getElementById('dropdown');
+    document.onclick = function(e){
+      if(e.target.id !== 'dropdown'){
+        //element clicked wasn't the div; hide the div
+        divToHide.style.display = 'none';
+      }
+    };
+  };
