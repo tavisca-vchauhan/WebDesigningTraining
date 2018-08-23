de0b45cfd10f6c22569c2d08fb8e4a8ab31d7ae4
Vishwas Chauhan <vchauhan@tavisca.com>
2018-08-23 17:22:47 +0530

    Autocomplete Version3

--- Script.js
+++ Script.js
@@ -1,8 +1,10 @@
+var index = -1;
 RegExp.escape = function (s) {
     return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 };
 function suggest() {
-    var arr = ["Pallavi", "Ragul", "Raja", "Anushka", "Bhargavi", "Swinal", "Utkarsh", "Saurabh", "Vishwas", "Paarth", "Gurbaksh", "Ashwarya", "Swapnil", "Ayush", "Shreea", "Smriti", "Shrijeet", "Ankita", "Rajpreet", "Neha", "Nihit", "Mohit"];
+
+    var arr = ["Ankita", "Anushka","Ashwarya","Ayush","Bhargavi","Gurbaksh","Mohit","Neha","Nihit","Paarth","Pallavi", "Ragul","Rajpreet", "Raja","Saurabh","Shreea","Smriti", "Shrijeet","Swapnil","Swinal","Utkarsh","Vishwas"];
     var count = 0;
     var input = document.getElementById('value');
     document.getElementById('list').innerHTML = "";
@@ -12,7 +14,7 @@ function suggest() {
     listItems = "";
     if (input.value) {
         count = 0;
-        dropdown.style.display = 'block';
+        dropdown.style.display ='block';
         listItems.size = 3;
         var showData = input.value;
         for (var i = 0; i < arr.length; i++) {
@@ -29,7 +31,7 @@ function suggest() {
         count++;
         var node = document.getElementById('dropdown')
         if (count > 5) {
-            node.style.overflow = "auto";
+            node.style.overflow="auto";
         }
         listItems = document.getElementById('list');
         var list = document.createElement("li");
@@ -44,23 +46,73 @@ function setVal(x) {
     document.getElementById('value').value = x;
     document.getElementById('dropdown').style.display = 'none';
 }
-window.onload = function(){
+document.getElementById("value").addEventListener("click",function () {
     var divToHide = document.getElementById('dropdown');
-    document.onclick = function(e){
-      if(e.target.id !== 'dropdown' && e.target.id !== 'value'){
-        divToHide.style.display = 'none';
-      }
-    var arrow=document.getElementById('list');
-    document.onkeypress = function(e)
-    {
-        if(e.keyCode== 40 )
-        {
-            arrow.style.focus='on';
+    document.onclick = function (e) {
+        if (e.target.id !== 'dropdown' && e.target.id !== 'value') {
+            divToHide.style.display = 'none';
         }
-    }
-    };
-  };
-  function Delete()
-  {
-    document.getElementById('value').value="";
-  }
+        else{
+            suggest();
+            index=-1;
+        }
+    }});
+    document.getElementById("value").addEventListener("keyup",function (e) {
+            if (e.which === 40 || e.which === 38 || e.which==13) {
+                var ul = document.getElementById('list');
+                var next = ul.getElementsByClassName('list-items');
+                var len = next.length;;
+                if (e.which === 40) {
+                    index++;
+                    if (next) {
+                        if (next.length - index > 0) 
+                        {
+                            next = ul.getElementsByClassName('list-items')[index];
+                            next.style.background = "#979797ab";
+                        }
+                        else 
+                        {
+                            next = ul.getElementsByClassName('list-items')[next.length-1];
+                            next.style.background = "#979797ab";
+                            index--;
+                        }
+                    } else {
+                        next = ul.getElementsByClassName('list-items')[0];
+                        next.style.background = "#979797ab";
+                        index = 0;
+                    }
+                    if(index>4)
+                    {
+                         document.getElementById("dropdown").scrollBy(0,30);                        
+                    }
+                } else if (e.which === 38) {
+                    index--;
+                    if (next) {
+                        if (index > -1) {
+                            next = ul.getElementsByClassName('list-items')[index];
+                            next.style.background = "#979797ab";
+                        }
+                        else {
+                            next = ul.getElementsByClassName('list-items')[0];
+                            next.style.background = "#979797ab";
+                            index++;
+                        }
+                    } else {
+                        next = ul.getElementsByClassName('list-items')[0];
+                        next.style.background = "#979797ab";
+                        index = 0;
+                    }
+                    if(index>0)
+                    {
+                         document.getElementById("dropdown").scrollBy(0,-30);                        
+                    }
+                }
+                else if(e.which==13)
+                {
+                    setVal(next[index].innerHTML);
+                }
+            }
+        });
+function Delete() {
+    document.getElementById('value').value = "";
+}
